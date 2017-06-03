69660d5b2e5117f167f44d989c9c33fccda1dcee
JoaooVictoorr <jvpiccoli@gmail.com>
2017-06-03 20:37:44 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoDeIA/AlgoritmoGenetico.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoDeIA
+{
+    class AlgoritmoGenetico
+    {
+        static Random r = new Random();
+        public void Avaliacao(Queue<Posicao> caminho)
+        {
+            var labirinto = Labirinto.getLabirinto();
+            Labirinto.Andar(labirinto, new Posicao { Linha = 1, Coluna = 0 }, caminho.Dequeue());
+
+            // Código de Avaliação do caminho
+        }
+
+        public static Stack<Posicao> Caminho(int quantDirecoes, Posicao posicaoAtual, Stack<Posicao> populacao)
+        {
+            for (int i = 0; i < quantDirecoes; i++)
+            {
+                populacao.Push(Sorteio(posicaoAtual));
+            }
+
+            return populacao;
+        }
+
+        public static Posicao Sorteio(Posicao posicaoAtual)
+        {
+            int resultado = RandomNumber();
+
+            if (resultado == 0)
+            {
+                Program.posicaoAtual = new Posicao(posicaoAtual.Linha, posicaoAtual.Coluna - 1);
+                return new Posicao { Linha = posicaoAtual.Linha, Coluna = posicaoAtual.Coluna - 1 };
+            }
+
+            if (resultado == 1)
+            {
+                Program.posicaoAtual = new Posicao(posicaoAtual.Linha - 1, posicaoAtual.Coluna);
+                return new Posicao { Linha = posicaoAtual.Linha - 1, Coluna = posicaoAtual.Coluna };
+            }
+
+            if (resultado == 2)
+            {
+                Program.posicaoAtual = new Posicao(posicaoAtual.Linha, posicaoAtual.Coluna + 1);
+                return new Posicao { Linha = posicaoAtual.Linha, Coluna = posicaoAtual.Coluna + 1 };
+            }
+
+            if (resultado == 3)
+            {
+                Program.posicaoAtual = new Posicao(posicaoAtual.Linha + 1, posicaoAtual.Coluna);
+                return new Posicao { Linha = posicaoAtual.Linha + 1, Coluna = posicaoAtual.Coluna };
+            }
+
+            return null;
+        }
+
+        private static int RandomNumber()
+        {
+            return r.Next(0, 3);
+        }
+    }
+}
---
+++ TrabalhoDeIA/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrabalhoDeIA
+{
+    class Program
+    {
+        public static Posicao posicaoAtual = new Posicao(1, 0);
+        static void Main(string[] args)
+        {
+            var labirinto = Labirinto.getLabirinto();
+            Stack<String[,]> pilhaLabirinto = new Stack<String[,]>();
+            Stack<Posicao> pilha = new Stack<Posicao>();
+            Random r = new Random();
+            Stack<Posicao> populacao1 = new Stack<Posicao>();
+
+            populacao1 = AlgoritmoGenetico.Caminho(10, posicaoAtual, populacao1);
+
+            do
+            {
+                var caminhos = Labirinto.CaminhosLivres(labirinto, posicaoAtual);
+
+                if (caminhos.Count > 0)
+                {
+                    var min = caminhos.Min(a => a.DistanciaHipotenusa);
+                    var caminhoEscolhido = caminhos.Where(a => a.DistanciaHipotenusa == min).FirstOrDefault();
+
+                    if (caminhos.Count >= 2)
+                        pilhaLabirinto.Push(labirinto);
+
+                    caminhos.Remove(caminhoEscolhido);
+
+                    foreach (var caminho in caminhos)
+                        pilha.Push(caminho);
+                    Console.Clear();
+                    Labirinto.Andar(labirinto, posicaoAtual, caminhoEscolhido);
+                }
+                else
+                {
+                    labirinto = pilhaLabirinto.Pop();
+                    var caminhoEscolhido = pilha.Pop();
+                    Console.Clear();
+                    Labirinto.Andar(labirinto, posicaoAtual, caminhoEscolhido);
+                }
+                Thread.Sleep(500);
+
+            } while (labirinto[19, 19] != "C");
+        }
+    }
+}
